04b4b2633828dea8fb9113fef3c02e4db1a1af99
dxr1988 <s.dxr@hotmail.com>
2015-07-30 11:58:16 -0400

    add more taggers

--- CompareTaggers.py
+++ CompareTaggers.py
@@ -3,25 +3,86 @@ Created on Jul 2, 2015
 
 @author: dongx
 '''
+
 import nltk
-from nltk.corpus import brown
-from nltk.corpus import treebank
-from nltk.tag import untag
-from nltk.tag import DefaultTagger
-from nltk.tag import UnigramTagger
+from nltk.corpus import brown, treebank
+from nltk.tag import untag, tnt, DefaultTagger, UnigramTagger, BigramTagger, TrigramTagger, brill, brill_trainer
 from nltk.corpus.reader import ChunkedCorpusReader
 
-
 sent = treebank.sents()[0]
-brown_train_sents = brown.tagged_sents(categories='news')[1000:]
+brown_train_sents = brown.tagged_sents(categories='news')[1001:]
 brown_test_sents = brown.tagged_sents(categories='news')[:1000]
+
+def backoff_tagger(tagged_sents, tagger_classes, backoff=None):
+    if not backoff:
+        backoff = tagger_classes[0](tagged_sents)
+        del tagger_classes[0]
+ 
+    for cls in tagger_classes:
+        tagger = cls(tagged_sents, backoff=backoff)
+        backoff = tagger
+ 
+    return backoff
+
+word_patterns = [
+    (r'^-?[0-9]+(.[0-9]+)?$', 'CD'),
+    (r'.*ould$', 'MD'),
+    (r'.*ing$', 'VBG'),
+    (r'.*ed$', 'VBD'),
+    (r'.*ness$', 'NN'),
+    (r'.*ment$', 'NN'),
+    (r'.*ful$', 'JJ'),
+    (r'.*ious$', 'JJ'),
+    (r'.*ble$', 'JJ'),
+    (r'.*ic$', 'JJ'),
+    (r'.*ive$', 'JJ'),
+    (r'.*ic$', 'JJ'),
+    (r'.*est$', 'JJ'),
+    (r'^a$', 'PREP'),
+]
+
+def train_brill_tagger(initial_tagger, train_sents, **kwargs):
+    templates = [
+        brill.Template(brill.Pos([-1])), #a rule can be generated using the previous part-of-speech tag
+        brill.Template(brill.Pos([1])),
+        brill.Template(brill.Pos([-2])),
+        brill.Template(brill.Pos([2])),
+        brill.Template(brill.Pos([-2, -1])),
+        brill.Template(brill.Pos([1, 2])),
+        brill.Template(brill.Pos([-3, -2, -1])),
+        brill.Template(brill.Pos([1, 2, 3])),
+        brill.Template(brill.Pos([-1]), brill.Pos([1])),
+        brill.Template(brill.Word([-1])),
+        brill.Template(brill.Word([1])),
+        brill.Template(brill.Word([-2])),
+        brill.Template(brill.Word([2])),
+        brill.Template(brill.Word([-2, -1])), #you can look at the combination of the previous two words to learn a transformation rule
+        brill.Template(brill.Word([1, 2])),
+        brill.Template(brill.Word([-3, -2, -1])),
+        brill.Template(brill.Word([1, 2, 3])),
+        brill.Template(brill.Word([-1]), brill.Word([1])),
+    ]
+    
+    trainer = brill_trainer.BrillTaggerTrainer(initial_tagger, templates, deterministic=True)
+    return trainer.train(train_sents, **kwargs)
+
+defaultTagger = DefaultTagger('NN')
+initialTagger = backoff_tagger(brown_train_sents, [UnigramTagger, BigramTagger, TrigramTagger], backoff=defaultTagger)
+brillTagger = train_brill_tagger(initialTagger, brown_train_sents)
+
+backoff = DefaultTagger('NN')
+backofftaggers = backoff_tagger(brown_train_sents, [UnigramTagger, BigramTagger,
+TrigramTagger], backoff=backoff)
 
 print("------------Recommended Tagger------------")
 print(nltk.pos_tag(sent))
-    
+
 print("------------Default Tagger------------")
-defaultTagger = DefaultTagger('NN')
 print(defaultTagger.tag(sent))
+
+print("------------Trigram Tagger------------")
+tritagger = TrigramTagger(brown_train_sents)
+print(tritagger.tag(sent))
     
 print("------------Unigram Tagger Untrained------------")
 unigramTagger = UnigramTagger(brown.tagged_sents(categories='news')[:1])
@@ -36,6 +97,14 @@ print("------------Unigram Tagger Trained------------")
 unigramTagger = UnigramTagger(brown_train_sents, cutoff=3)
 print(unigramTagger.tag(treebank.sents()[0]))
 
+print("------------Accuracy: Bigram Tagger Trained------------")
+bitagger = BigramTagger(brown_train_sents)
+print(bitagger.evaluate(brown_test_sents))
+
+print("------------Accuracy: Trigram Tagger Trained------------")
+tritagger = TrigramTagger(brown_train_sents)
+print(tritagger.evaluate(brown_test_sents))
+
 print("------------Accuracy: Unigram Tagger Trained------------")
 unigramTagger = UnigramTagger(brown_train_sents)
 print(unigramTagger.evaluate(brown_test_sents))
@@ -44,6 +113,18 @@ print("------------Accuracy: Unigram Tagger Trained with cutoff = 3------------"
 unigramTagger = UnigramTagger(brown_train_sents, cutoff = 3)
 print(unigramTagger.evaluate(brown_test_sents))
 
-print("------------Accuracy: Unigram Tagger Trained with Default Tagger as a backoff tagger------------")
+print("------------Accuracy: Unigram Tagger with backoff enabled. Backoff Chain: UnigramTagger -> DefaultTagger------------")
 unigramTagger = UnigramTagger(brown_train_sents, backoff=defaultTagger)
 print(unigramTagger.evaluate(brown_test_sents))
+
+print("------------Accuracy: Tagger with backoff enabled. Backoff Chain: TrigramTagger -> BigramTagger -> UnigramTagger------------")
+print(backofftaggers.evaluate(brown_test_sents))
+
+print("------------Accuracy: Brill Tagger------------")
+print(brillTagger.evaluate(brown_test_sents))
+print(brillTagger.rules())
+
+print("------------Accuracy: TnT Tagger------------")
+tnt_tagger = tnt.TnT(N=100)
+tnt_tagger.train(brown_train_sents)
+print(tnt_tagger.evaluate(brown_test_sents))
--- TagPattern.py
+++ TagPattern.py
@@ -4,8 +4,10 @@ Created on Jun 19, 2015
 @author: dongx
 '''
 import nltk
+import nltk.corpus, nltk.tag, itertools
 from nltk.tree import *
 from nltk import word_tokenize,Text,pos_tag
+from nltk.tag import brill, brill_trainer, DefaultTagger, UnigramTagger, BigramTagger, TrigramTagger
 
 def ExtractPhrases( myTree, myLabel):
     myPhrases = []
@@ -17,6 +19,21 @@ def ExtractPhrases( myTree, myLabel):
             if (len(list_of_phrases) > 0):
                 myPhrases.extend(list_of_phrases)
     return myPhrases
+
+brown_review_sents = nltk.corpus.brown.tagged_sents(categories=['reviews'])
+brown_lore_sents = nltk.corpus.brown.tagged_sents(categories=['lore'])
+brown_romance_sents = nltk.corpus.brown.tagged_sents(categories=['romance'])
+ 
+brown_train = list(itertools.chain(brown_review_sents[:1000], brown_lore_sents[:1000], brown_romance_sents[:1000]))
+brown_test = list(itertools.chain(brown_review_sents[1000:2000], brown_lore_sents[1000:2000], brown_romance_sents[1000:2000]))
+ 
+conll_sents = nltk.corpus.conll2000.tagged_sents()
+conll_train = list(conll_sents[:4000])
+conll_test = list(conll_sents[4000:8000])
+ 
+treebank_sents = nltk.corpus.treebank.tagged_sents()
+treebank_train = list(treebank_sents[:1500])
+treebank_test = list(treebank_sents[1500:3000])
 
 sentences = ["Why would you go to school when you could work and earn money?",
              "the little yellow dogs barked at the cat.",
@@ -38,13 +55,75 @@ grammar = '''
              NPS: {<NP><P><NP>(<P><NP>)*}
              VP:{(<V><JJ.*>+<P>)|(<V><P>?)}
           '''
+          
+def backoff_tagger(tagged_sents, tagger_classes, backoff=None):
+    if not backoff:
+        backoff = tagger_classes[0](tagged_sents)
+        del tagger_classes[0]
+ 
+    for cls in tagger_classes:
+        tagger = cls(tagged_sents, backoff=backoff)
+        backoff = tagger
+ 
+    return backoff
+
+word_patterns = [
+    (r'^-?[0-9]+(.[0-9]+)?$', 'CD'),
+    (r'.*ould$', 'MD'),
+    (r'.*ing$', 'VBG'),
+    (r'.*ed$', 'VBD'),
+    (r'.*ness$', 'NN'),
+    (r'.*ment$', 'NN'),
+    (r'.*ful$', 'JJ'),
+    (r'.*ious$', 'JJ'),
+    (r'.*ble$', 'JJ'),
+    (r'.*ic$', 'JJ'),
+    (r'.*ive$', 'JJ'),
+    (r'.*ic$', 'JJ'),
+    (r'.*est$', 'JJ'),
+    (r'^a$', 'PREP'),
+]
+
+def train_brill_tagger(initial_tagger, train_sents, **kwargs):
+    templates = [
+        brill.Template(brill.Pos([-1])), #a rule can be generated using the previous part-of-speech tag
+        brill.Template(brill.Pos([1])),
+        brill.Template(brill.Pos([-2])),
+        brill.Template(brill.Pos([2])),
+        brill.Template(brill.Pos([-2, -1])),
+        brill.Template(brill.Pos([1, 2])),
+        brill.Template(brill.Pos([-3, -2, -1])),
+        brill.Template(brill.Pos([1, 2, 3])),
+        brill.Template(brill.Pos([-1]), brill.Pos([1])),
+        brill.Template(brill.Word([-1])),
+        brill.Template(brill.Word([1])),
+        brill.Template(brill.Word([-2])),
+        brill.Template(brill.Word([2])),
+        brill.Template(brill.Word([-2, -1])), #you can look at the combination of the previous two words to learn a transformation rule
+        brill.Template(brill.Word([1, 2])),
+        brill.Template(brill.Word([-3, -2, -1])),
+        brill.Template(brill.Word([1, 2, 3])),
+        brill.Template(brill.Word([-1]), brill.Word([1])),
+    ]
+    
+    trainer = brill_trainer.BrillTaggerTrainer(initial_tagger, templates, deterministic=True)
+    return trainer.train(train_sents, **kwargs)
+
+default_tagger = DefaultTagger('NN')
+initial_tagger = backoff_tagger(brown_train, [UnigramTagger, BigramTagger, TrigramTagger], backoff=default_tagger)
+brill_tagger = train_brill_tagger(initial_tagger, brown_train)
+
 #TARGET: {<V><NP>?<JJ.*>*<P>?}
 for sentence in sentences:    
     words = nltk.word_tokenize(sentence)
     tags = nltk.pos_tag(words)
+    brill_tags = brill_tagger.tag(words)
     cp = nltk.RegexpParser(grammar)
     result = cp.parse(tags)
+    brill_result = cp.parse(brill_tags)
     print(result)
+    print(brill_result)
+    '''
     list_np = ExtractPhrases(result, 'NP')
     list_jp = ExtractPhrases(result, 'JP')
     list_jj = ExtractPhrases(result, 'JJ')
@@ -58,4 +137,6 @@ for sentence in sentences:
         
     for jj in list_jj:
         print(jj)
+    '''
     result.draw()
+    brill_result.draw()
